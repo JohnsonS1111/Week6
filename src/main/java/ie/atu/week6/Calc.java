af792051a00591278a6f38f756ff0e69f1aec11a
Johnson <g00389668@atu.ie>
2022-10-26 13:00:46 +0100

    Added new files "Calc and CalcTest"

---
+++ src/main/java/ie/atu/week6/Calc.java
@@ -0,0 +1,8 @@
+package ie.atu.week6;
+
+public class Calc {
+    private int ans;
+
+    public Calc(){ ans = 0; }
+
+}
---
+++ src/test/java/ie/atu/week6/CalcTest.java
@@ -0,0 +1,17 @@
+package ie.atu.week6;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalcTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+}
