9f1b51073c8d4163c12e84271ed7c19e3977b32b
Johnson <g00389668@atu.ie>
2022-10-26 13:36:39 +0100

    Added new files "Calc and CalcTest"

--- src/main/java/ie/atu/week6/Calc.java
+++ src/main/java/ie/atu/week6/Calc.java
@@ -5,4 +5,23 @@ public class Calc {
 
     public Calc(){ ans = 0; }
 
+    public int Add(int a, int b){
+        ans = a + b;
+        return ans;
+    }
+
+    public  int Subtract(int a, int b){
+        ans = a - b;
+        return ans;
+    }
+
+    public int Multiply(int a, int b){
+        ans = a * b;
+        return ans;
+    }
+
+    public int Divide(int a, int b){
+        ans = a/b;
+        return ans;
+    }
 }
